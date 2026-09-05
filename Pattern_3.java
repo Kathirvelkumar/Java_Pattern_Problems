3dc1d332e264af4e6800ef10b5b11e914e926cf6
Kathirvel K <146557018+Kathirvelkumar@users.noreply.github.com>
2026-09-05 22:34:13 +0530

    Add pattern printing program in Pattern_3.java
    Implemented a Java program to print a number pattern based on user input.

---
+++ Pattern_3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+
+/*
+Given an integer n. 
+You need to recreate the pattern given below for any value of N.
+Let's say for N = 5, the pattern should look like as below:
+
+1
+12
+123
+1234
+12345
+
+*/
