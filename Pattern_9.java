28d4affe24878c4226a3c69d47854e98e95a9226
Kathirvel K <146557018+Kathirvelkumar@users.noreply.github.com>
2026-09-05 22:46:26 +0530

    Add pattern printing functionality in Pattern_9.java
    Implement pattern printing for a given integer n.

---
+++ Pattern_9.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+      // Lower half
+        for (int i = n; i >= 1; i--) {
+            // Spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+/*
+Given an integer n. 
+You need to recreate the pattern given below for any value of N. 
+Let's say for N = 5, the pattern should look like as below:
+
+    * 
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
